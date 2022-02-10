ca1fce8c46d6950cca4866d7ca883da0af65215a
MWebbimus <marco@webb.hk>
2022-02-10 16:02:11 +0800

    Completed Problem J4

--- ProblemJ4.py
+++ ProblemJ4.py
@@ -17,22 +17,26 @@ for x in range(0,len(bookOrder)):
 
 # numOrder is the array containing the "bookshelf"
 # where L = 3, M = 2, and S = 1
-#]]
-for x in range(0,len(numOrder)-1):
-    if numOrder[x] < numOrder[x + 1]:
-        z = numOrder[x]
-        numOrder[x] = numOrder[x + 1]
-        numOrder[x + 1] = z
-        count = count + 1
+
+while correctOrder == False:
+    c = 0
+    for x in range(0,len(numOrder)-1):
+        if numOrder[x] < numOrder[x + 1]:
+            z = numOrder[x]
+            numOrder[x] = numOrder[x + 1]
+            numOrder[x + 1] = z
+            count = count + 1
 
 # numOrder organized
-for x in range(0,len(numOrder)-1):
-    if numOrder[x] < numOrder[x+1]:
-        print("bing")
-        break
-    if x == (len(numOrder) - 1):
-        correctOrder = True
-        print("wong")
+    for x in range(0,len(numOrder)-1):
+        if numOrder[x] < numOrder[x+1]:
+            print("bing")
+            break
+        if c == (len(numOrder) - 2):
+            print("wong")
+            correctOrder = True
+        else:
+            c = c + 1
     
 print(count)
         
