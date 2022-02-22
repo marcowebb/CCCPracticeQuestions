76732f23b6d07bb954d56d773241c278a91cd857
MWebbimus <marco@webb.hk>
2022-02-22 08:01:30 +0800

    fixed

--- ProblemJ5.py
+++ ProblemJ5.py
@@ -12,35 +12,60 @@
 # array for flipped collumns
 # counter for gold tiles
 
+goldTiles = 0
+
 canvasWidth = int(input())
 flippedRowsCount = 0
-flippedRows = [0] * canvasWidth # 0 is unflipped, 1 is flipped
+flippedRows = [] # 0 is unflipped, 1 is flipped
 canvasHeight = int(input())
 flippedCollumnsCount = 0
-flippedCollumns = [0] * canvasHeight # 0 is unflipped, 1 is flipped
+flippedCollumns = [] # 0 is unflipped, 1 is flipped
 actionsTaken = int(input())
 
 # gold = (number of collumns * collumn height) - number of rows 
 # PLUS
 # (number of rows * row length) - number of collumns
 
-for i in range(0, actionsTaken):
+# for i in range(0, actionsTaken):
+#     axisDesignation, rcNumber = [i for i in input().split()]
+#     rcNumber = int(rcNumber)
+#     if axisDesignation == "C" and flippedCollumns[rcNumber - 1] != 1:
+#         flippedCollumns[rcNumber - 1] = 1
+#         flippedCollumnsCount += 1
+#     elif axisDesignation == "C" and flippedCollumns[rcNumber - 1] == 1:
+#         flippedCollumns[rcNumber - 1] = 0
+#         flippedCollumnsCount -= 1
+#     elif axisDesignation == "R" and flippedCollumns[rcNumber - 1] != 1:
+#         flippedRows[rcNumber - 1] = 1
+#         flippedRowsCount += 1
+#     else:
+#         flippedRows[rcNumber - 1] = 0
+#         flippedRowsCount -= 1
+
+for x in range(0,actionsTaken):
+    flippedRow = False
+    flippedColumn = False
+
     axisDesignation, rcNumber = [i for i in input().split()]
     rcNumber = int(rcNumber)
-    if axisDesignation == "C" and flippedCollumns[rcNumber - 1] != 1:
-        flippedCollumns[rcNumber - 1] = 1
-        flippedCollumnsCount += 1
-    elif axisDesignation == "C" and flippedCollumns[rcNumber - 1] == 1:
-        flippedCollumns[rcNumber - 1] = 0
-        flippedCollumnsCount -= 1
-    elif axisDesignation == "R" and flippedCollumns[rcNumber - 1] != 1:
-        flippedRows[rcNumber - 1] = 1
-        flippedRowsCount += 1
-    else:
-        flippedRows[rcNumber - 1] = 0
-        flippedRowsCount -= 1
 
-goldTiles = 0
-goldTiles = (flippedCollumnsCount * canvasHeight) - flippedRowsCount
-goldTiles += (flippedRowsCount * canvasWidth) - flippedCollumnsCount
+    if axisDesignation == "R":
+        for x in flippedRows:
+            if rcNumber == x:
+                flippedRow = True
+        if flippedRow == False:
+            flippedRows.append(rcNumber)
+            flippedRowsCount += 1
+            goldTiles += canvasHeight - flippedCollumnsCount
+        elif flippedRow == True:
+            flippedRows.remove(rcNumber)
+            flippedRowsCount -= 1
+            goldTiles -= canvasHeight - flippedCollumnsCount
+
+
+
+
+# goldTiles = 0
+# goldTiles = (flippedCollumnsCount * canvasHeight) - flippedRowsCount
+# goldTiles += (flippedRowsCount * canvasWidth) - flippedCollumnsCount
 print(goldTiles)
